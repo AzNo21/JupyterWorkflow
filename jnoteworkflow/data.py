34fef2ffde350786abc9eff999f5a3633e6f797d
Noorani Azad <azad.noorani@afconsult.com>
2018-05-13 19:30:01 +0200

    move function to package called jnoteworkflow

---
+++ jnoteworkflow/data.py
@@ -0,0 +1,31 @@
+import os
+from urllib.request import urlretrieve
+import pandas as pd
+
+Fremont_URL = 'https://data.seattle.gov/api/views/65db-xm6k/rows.csv?accessType=DOWNLOAD'
+
+
+def get_Fremont_data(filename='Fremont.csv', url=Fremont_URL, force_download=False):
+    '''
+    Download and cache the Fremont Data
+
+    Parameters
+    ----------
+    filename: string (optional)
+        location to save the data
+    url: string (optional)
+        web location of the data
+    force_download: bool (optional)
+        if True, force re-download of the data
+
+    Returns
+    -------
+    data: pandas.Dataframe
+        the Fremont bridge data
+    '''
+    if force_download or not os.path.exists(filename):
+        urlretrieve(url, filename)
+    data = pd.read_csv('Fremont.csv', index_col='Date', parse_dates=True)
+    data.columns = ["West", "East"]
+    data['Total'] = data['West'] + data['East']
+    return data
