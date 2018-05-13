10e85cfab8ed21c6a3e36b97d752913dfa594f92
Noorani Azad <azad.noorani@afconsult.com>
2018-05-13 23:58:30 +0200

    refactoring the datetime index to speed up formatting process, explanation added to scratch.py

--- jnoteworkflow/data.py
+++ jnoteworkflow/data.py
@@ -5,7 +5,8 @@ import pandas as pd
 Fremont_URL = 'https://data.seattle.gov/api/views/65db-xm6k/rows.csv?accessType=DOWNLOAD'
 
 
-def get_Fremont_data(filename='Fremont.csv', url=Fremont_URL, force_download=False):
+def get_Fremont_data(filename='Fremont.csv', url=Fremont_URL,
+                     force_download=False):
     '''
     Download and cache the Fremont Data
 
@@ -25,7 +26,13 @@ def get_Fremont_data(filename='Fremont.csv', url=Fremont_URL, force_download=Fal
     '''
     if force_download or not os.path.exists(filename):
         urlretrieve(url, filename)
-    data = pd.read_csv('Fremont.csv', index_col='Date', parse_dates=True)
+    data = pd.read_csv('Fremont.csv', index_col='Date')
+
+    try:
+        data.index = pd.to_datetime(data.index, format='%m/%d/%Y %H:%M:%S %p')
+    except TypeError:
+        data.index = pd.to_datetime(data.index)
+
     data.columns = ["West", "East"]
     data['Total'] = data['West'] + data['East']
     return data
