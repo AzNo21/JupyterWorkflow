04bb901b7c38579e36725f3bb9186d9ecc27a481
Noorani Azad <azad.noorani@afconsult.com>
2018-05-13 22:35:55 +0200

    adding the unit test functions v01

---
+++ jnoteworkflow/tests/test_data.py
@@ -0,0 +1,6 @@
+from jnoteworkflow.data import get_Fremont_data
+import pandas as pd
+def test_Fremont_data():
+    data = get_Fremont_data()
+    assert all(data.columns == ['West', 'East', 'Total'])
+    assert isinstance(data.index, pd.DatetimeIndex)
