840c9821b0f9158ffa36d158bada2cd0ca091e6a
Noorani Azad <azad.noorani@afconsult.com>
2018-05-14 13:27:06 +0200

    debugging the broken data.py function

--- jnoteworkflow/tests/test_data.py
+++ jnoteworkflow/tests/test_data.py
@@ -1,6 +1,8 @@
 from jnoteworkflow.data import get_Fremont_data
 import pandas as pd
+import numpy as np
 def test_Fremont_data():
     data = get_Fremont_data()
     assert all(data.columns == ['West', 'East', 'Total'])
     assert isinstance(data.index, pd.DatetimeIndex)
+    assert len(np.unique(data.index.time) == 24)   
